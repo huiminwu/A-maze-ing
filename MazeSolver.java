38897ee82a3d18c066a40c8a36f300f8ae40a8cc
MilanHaiman <mhaiman@stuy.edu>
2018-04-12 20:48:14 -0400

    Finish Maze Solver, add mazes

--- MazeSolver.java
+++ MazeSolver.java
@@ -13,13 +13,21 @@ public class MazeSolver {
             return true;
         }
         Maze snapshot = new Maze(maze);
+        // displayer.atTopOfWindow(snapshot.toString() + "snapshot initial");
         for(Direction direction : Direction.values()) {
             maze.dropA(maze.WALL);
+            // displayer.atTopOfWindow(maze.toString() + "drop WALL");
+            // displayer.atTopOfWindow(snapshot.toString() + "snapshot after drop WALL");
             maze.go(direction);
+            // displayer.atTopOfWindow(maze.toString() + "went " + direction); // the real one
+
+            // displayer.atTopOfWindow(snapshot.toString() + "snapshot after went " + direction);
             if(solve(maze)) {
                 return true;
             }
-            maze = snapshot;
+            maze = new Maze(snapshot);
+            // displayer.atTopOfWindow(maze.toString() + "restored snapshot");
+            // displayer.atTopOfWindow(snapshot.toString() + "snapshot after restored snapshot");
         }
         return false;
     }
